f797b3ed0fb87c4f8ed0f03f099b7fd1caf09145
Shri-0 <shrinath.narayanan@gmail.com>
2023-09-18 16:22:10 -0700

    "change"

---
+++ Table Extraction/start.py
@@ -0,0 +1,10 @@
+import pandas as pd
+
+#this gets basic data from a wikipedia table. Will get the viwership for each season of the simpsons
+'''
+simpsons = pd.read_html(
+    'https://en.wikipedia.org/wiki/List_of_The_Simpsons_episodes_(seasons_1%E2%80%9320)')
+
+#print(len(simpsons))
+print(simpsons[1])
+'''
